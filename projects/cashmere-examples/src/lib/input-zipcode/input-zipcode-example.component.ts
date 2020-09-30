3d71f2eb1844f975b00dfd7ffe2fde71e0cfc7ae
Barry Cann <bacann@wcf.com>
2020-09-30 15:21:57 -0600

    fix(input): WCASHMERE-199: Fixed a whitespace error

--- projects/cashmere-examples/src/lib/input-zipcode/input-zipcode-example.component.ts
+++ projects/cashmere-examples/src/lib/input-zipcode/input-zipcode-example.component.ts
@@ -7,9 +7,7 @@ import {FormControl, Validators} from '@angular/forms';
   styleUrls: ['./input-zipcode-example.component.scss']
 })
 export class InputZipcodeExampleComponent {
-
     formDemoZip = new FormControl('', [Validators.pattern(/(^\d{5}$)|(^\d{5}-\d{4}$)/), Validators.required]);
     formDemoZipInitVal = new FormControl('840705313', [Validators.pattern(/(^\d{5}$)|(^\d{5}-\d{4}$)/), Validators.required]);
     formDemoZipFormattedInitVal = new FormControl('84070-5313', [Validators.pattern(/(^\d{5}$)|(^\d{5}-\d{4}$)/), Validators.required]);
-    
 }
