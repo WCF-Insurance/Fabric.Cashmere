511ad547c88a0fa1cac7eca5b53027d31dc5812b
Travis Foxley <tfoxley@wcf.com>
2020-11-16 08:12:55 -0700

    fix(angular8): fix issues with angular 8 upgrade

--- projects/cashmere-examples/src/project-template/src/polyfills.ts
+++ projects/cashmere-examples/src/project-template/src/polyfills.ts
@@ -31,8 +31,7 @@ import 'core-js/es/string';
 import 'core-js/es/array';
 /** Evergreen browsers require these. **/
 // Used for reflect-metadata in JIT. If you use AOT (and only Angular decorators), you can remove.
-import 'core-js/es6/reflect';
-import 'core-js/es7/reflect';
+import 'core-js/es/reflect';
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
