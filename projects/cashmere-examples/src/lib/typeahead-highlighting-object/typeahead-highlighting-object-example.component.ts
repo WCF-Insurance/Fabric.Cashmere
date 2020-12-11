59bbabdbbd73dc57059ed29c39f385850ee6da95
Travis Foxley <tfoxley@wcf.com>
2020-12-11 11:36:44 -0700

    fix(typeahead): fix issues with disabled state
    Clicking on the typeahead even when it is disabled caused the results to be revealed. Now the
    results will not be shown and the input cannot be focused when the component is disabled.

--- projects/cashmere-examples/src/lib/typeahead-highlighting-object/typeahead-highlighting-object-example.component.ts
+++ projects/cashmere-examples/src/lib/typeahead-highlighting-object/typeahead-highlighting-object-example.component.ts
@@ -10,49 +10,62 @@ export class TypeaheadHighlightingObjectExampleComponent implements OnInit {
     form: FormGroup;
     filteredData: object[] = [];
     typeaheadData = [
-        {'name': 'Alabama',
-         'year': 1819,
-         'flower': 'Camellia'
+        {
+            'name': 'Alabama',
+            'year': 1819,
+            'flower': 'Camellia'
         },
-        {'name': 'Alaska',
-        'year': 1959,
-        'flower': 'Forget-Me-Not'
+        {
+            'name': 'Alaska',
+            'year': 1959,
+            'flower': 'Forget-Me-Not'
         },
-        {'name': 'Arizona',
-        'year': 1912,
-        'flower': 'Saguaro Blossom'
+        {
+            'name': 'Arizona',
+            'year': 1912,
+            'flower': 'Saguaro Blossom'
         },
-        {'name': 'Arkansas',
-        'year': 1836,
-        'flower': 'Apple Blossom'
+        {
+            'name': 'Arkansas',
+            'year': 1836,
+            'flower': 'Apple Blossom'
         },
-        {'name': 'California',
-        'year': 1850,
-        'flower': 'California Poppy'
+        {
+            'name': 'California',
+            'year': 1850,
+            'flower': 'California Poppy'
         },
-        {'name': 'Colorado',
-        'year': 1876,
-        'flower': 'Blue Columbine'
+        {
+            'name': 'Colorado',
+            'year': 1876,
+            'flower': 'Blue Columbine'
         },
-        {'name': 'Connecticut',
-        'year': 1788,
-        'flower': 'Mountain Laurel'
+        {
+            'name': 'Connecticut',
+            'year': 1788,
+            'flower': 'Mountain Laurel'
         },
-        {'name': 'Delaware',
-        'year': 1787,
-        'flower': 'Peach Blossom'
+        {
+            'name': 'Delaware',
+            'year': 1787,
+            'flower': 'Peach Blossom'
         },
-        {'name': 'Florida',
-        'year': 1845,
-        'flower': 'Orange Blossom'
+        {
+            'name': 'Florida',
+            'year': 1845,
+            'flower': 'Orange Blossom'
         },
-        {'name': 'Georgia',
-        'year': 1788,
-        'flower': 'Cherokee Rose'
+        {
+            'name': 'Georgia',
+            'year': 1788,
+            'flower': 'Cherokee Rose'
         }
     ];
 
     typedChars = '';
+
+    disabled = false;
+    buttonLabel = 'Disable';
 
     constructor(private fb: FormBuilder) {
     }
@@ -68,7 +81,7 @@ export class TypeaheadHighlightingObjectExampleComponent implements OnInit {
         this.setValue(term);
         if (term) {
             this.filteredData = this.typeaheadData.filter(item => item.name.toLowerCase().indexOf(term.toLowerCase()) > -1
-            || item.flower.toLowerCase().indexOf(term.toLowerCase()) > -1);
+                || item.flower.toLowerCase().indexOf(term.toLowerCase()) > -1);
         } else {
             this.filteredData = this.typeaheadData;
         }
@@ -80,15 +93,20 @@ export class TypeaheadHighlightingObjectExampleComponent implements OnInit {
 
     formatDisplay(item) {
         if (!item) {
-          return '';
+            return '';
         }
         return item.name + ' - ' + item.year;
-      }
+    }
 
     private setValue(item) {
         const control = this.form.get('item');
         if (control) {
             control.setValue(item);
         }
+    }
+
+    toggleDisabled() {
+        this.disabled = !this.disabled;
+        this.buttonLabel = this.disabled ? 'Enable' : 'Disable';
     }
 }
