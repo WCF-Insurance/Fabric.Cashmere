5f6deaa686aebe9d971eb75db6034247a52d50fa
Travis Foxley <tfoxley@wcf.com>
2021-04-07 10:27:08 -0600

    fix(timepicker): better support for reactive forms

--- projects/cashmere-examples/src/lib/datepicker/datepicker-example.component.ts
+++ projects/cashmere-examples/src/lib/datepicker/datepicker-example.component.ts
@@ -1,13 +1,27 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup} from '@angular/forms';
 
 @Component({
     selector: 'hc-datepicker-example',
     templateUrl: './datepicker-example.component.html',
     styleUrls: ['datepicker-example.component.scss']
 })
-export class DatepickerExampleComponent {
-    date1 = new Date(2010, 1, 1);
-    date2 = new Date();
-    date3 = new Date("2010-01-01T20:15:00.00");
-    hourCycle = false;
+export class DatepickerExampleComponent implements OnInit {
+    date = new Date(2010, 1, 1);
+    time = new Date();
+    dateTime = new Date("2010-01-01T20:15:00.00");
+
+    form: FormGroup;
+
+    constructor(private fb: FormBuilder) {
+    }
+
+    ngOnInit(): void {
+        this.form = this.fb.group({
+            date: [this.date],
+            time: [this.time],
+            dateTime: [this.dateTime],
+            hourCycle: [false]
+        });
+    }
 }
